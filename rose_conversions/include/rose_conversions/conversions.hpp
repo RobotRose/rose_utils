4b014f9b5b2c88ef3f262e659f21a3ba902a7d43
Mathijs de Langen <langen@robot-rose.nl>
2015-02-03 17:45:27 +0100

    fixing references for utils

--- rose_conversions/include/rose_conversions/conversions.hpp
+++ rose_conversions/include/rose_conversions/conversions.hpp
@@ -13,9 +13,53 @@
 #ifndef CONVERSIONS_HPP
 #define CONVERSIONS_HPP
 
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
+#include <fcntl.h>
+#include <iostream>
+#include <sstream>
+#include <stdio.h>
+#include <string>
+#include <termios.h>
+
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Point32.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Vector3.h>
+#include <geometry_msgs/Quaternion.h>
+
+using geometry_msgs::Point32;
+using geometry_msgs::Point;
+using geometry_msgs::Pose;
+using geometry_msgs::PoseStamped;
+
+using std::string;
+using std::stringstream;
+
 namespace rose_conversions { 
 
+string  ReplaceString(string subject, const string& search, const string& replace);
+string  IntToString(int number);
 
+string  floatToString(float number);
+string  doubleToString(double number);
+
+int     StringToInt(string number_string);
+double  StringToDouble(string number_string);
+
+double  floatToDouble( float x );
+float   doubleToFloat( double x );
+
+int     kbhit();
+
+geometry_msgs::Vector3      quaternionToRPY(const geometry_msgs::Quaternion& quat);
+geometry_msgs::Vector3      quaternionToRPY(const tf::Quaternion& quat);
+geometry_msgs::Quaternion   RPYToQuaterion( const geometry_msgs::Vector3& rpy);
+geometry_msgs::Quaternion   RPYToQuaterion( const double& roll, const double& pitch, const double& yaw);
+
+Point           point32ToPoint( const Point32 point32 );
+Point32         pointToPoint32( const Point point );
 
 } // rose_conversions
 
--- rose_conversions/src/conversions.cpp
+++ rose_conversions/src/conversions.cpp
@@ -14,8 +14,155 @@
 
 namespace rose_conversions { 
 
+string ReplaceString(string subject, const string& search, const string& replace) {
+    size_t pos = 0;
+    while ((pos = subject.find(search, pos)) != string::npos) {
+         subject.replace(pos, search.length(), replace);
+         pos += replace.length();
+    }
+    return subject;
+}
+
+string IntToString(int number)
+{
+   stringstream ss;//create a stringstream
+   ss << number;//add number to the stream
+   return ss.str();//return a string with the contents of the stream
+}
+
+string floatToString(float number)
+{
+   stringstream ss;//create a stringstream
+   ss << number;//add number to the stream
+   return ss.str();//return a string with the contents of the stream
+}
+
+
+string doubleToString(double number)
+{
+   stringstream ss;//create a stringstream
+   ss << number;//add number to the stream
+   return ss.str();//return a string with the contents of the stream
+}
+
+int StringToInt(string number_string)
+{
+    int x = 0;
+    try {
+        x = boost::lexical_cast<int>(number_string);
+    } catch( boost::bad_lexical_cast const& ) {
+        throw;
+    }
+    return x;
+}
+
+double StringToDouble(string number_string)
+{
+    double x = 0;
+    try {
+        x = boost::lexical_cast<double>(number_string);
+    } catch( boost::bad_lexical_cast const& ) {
+        throw;
+    }
+    return x;
+}
+
+double floatToDouble( float x )
+{
+    return (double)x;
+}
+
+float doubleToFloat( double x )
+{
+    return (float)x;
+}
+
+int kbhit(void)
+{
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+ 
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+ 
+  ch = getchar();
+ 
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+ 
+  if(ch != EOF)
+  {
+    ungetc(ch, stdin);
+    return 1;
+  }
+ 
+  return 0;
+}
+
+// Convert quaternion to RPY
+geometry_msgs::Vector3 quaternionToRPY(const geometry_msgs::Quaternion& quat)
+{
+    geometry_msgs::Vector3 vector;
+    tf::Quaternion q;
+    tf::quaternionMsgToTF(quat, q);
+    return quaternionToRPY(q);
+}
+
+geometry_msgs::Vector3 quaternionToRPY(const tf::Quaternion& quat)
+{
+    geometry_msgs::Vector3 vector;
+    tf::Matrix3x3(quat).getRPY(vector.x, vector.y, vector.z);
+    return vector;
+}
+
+geometry_msgs::Quaternion RPYToQuaterion( const geometry_msgs::Vector3& rpy)
+{
+    return tf::createQuaternionMsgFromRollPitchYaw(rpy.x, rpy.y, rpy.z);;
+}
+
+geometry_msgs::Quaternion RPYToQuaterion( const double& roll, const double& pitch, const double& yaw)
+{
+    return tf::createQuaternionMsgFromRollPitchYaw(roll, pitch, yaw);
+}
+
+const char* byteToBinary(int x)
+{
+    static char b[9];
+    b[0] = '\0';
+
+    int z;
+    for (z = 128; z > 0; z >>= 1)
+    {
+        strcat(b, ((x & z) == z) ? "1" : "0");
+    }
+
+    return b;
+}
+
+geometry_msgs::Point point32ToPoint( const geometry_msgs::Point32 point32 )
+{
+    geometry_msgs::Point point;
+    point.x = floatToDouble(point32.x);
+    point.y = floatToDouble(point32.y);
+    point.z = floatToDouble(point32.z);
+
+    return point;
+}
+
+geometry_msgs::Point32 pointToPoint32( const geometry_msgs::Point point )
+{
+    geometry_msgs::Point32 point32;
+    point32.x = doubleToFloat(point.x);
+    point32.y = doubleToFloat(point.y);
+    point32.z = doubleToFloat(point.z);
+
+    return point32;
+}
 
 
 } // rose_conversions
-
-#endif // CONVERSIONS_HPP
