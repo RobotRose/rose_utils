2fe3a179b9e4dbfdddc5c5958eb9d5f345d1cf55
Mathijs de Langen <langen@robot-rose.nl>
2015-02-03 17:30:46 +0100

    begin of rose conversions

---
+++ rose_conversions/include/rose_conversions/conversions.hpp
@@ -0,0 +1,22 @@
+/***********************************************************************************
+* Copyright: Rose B.V. (2015)
+*
+* Revision History:
+*	Author: Mathijs de Langen
+*	Date  : 2015/02/03
+* 		- File created.
+*
+* Description:
+*	Conversions
+* 
+***********************************************************************************/
+#ifndef CONVERSIONS_HPP
+#define CONVERSIONS_HPP
+
+namespace rose_conversions { 
+
+
+
+} // rose_conversions
+
+#endif // CONVERSIONS_HPP
---
+++ rose_conversions/src/conversions.cpp
@@ -0,0 +1,21 @@
+/***********************************************************************************
+* Copyright: Rose B.V. (2015)
+*
+* Revision History:
+*	Author: Mathijs de Langen
+*	Date  : 2015/02/03
+* 		- File created.
+*
+* Description:
+*	Conversions
+* 
+***********************************************************************************/
+#include "rose_conversions/conversions.hpp"
+
+namespace rose_conversions { 
+
+
+
+} // rose_conversions
+
+#endif // CONVERSIONS_HPP
